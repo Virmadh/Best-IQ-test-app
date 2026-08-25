fe66992af1bf19b8d820f85bf67149bd954b3896
Virmadh <Virmadh2@icloud.com>
2026-08-25 21:59:40 +0000

    added questions

--- iq-test-app/app/(tabs)/testpage.tsx
+++ iq-test-app/app/(tabs)/testpage.tsx
@@ -2,25 +2,56 @@ import { Image ,StyleSheet } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { View, Text, Pressable } from "react-native";
+import data from '@/assets/data/iq_test_30_hard_questions.json';
 //npm start
 export default function TestPage() {
+  const questions = data.questions;
+  console.log(questions[0].question);
   return (
     <ThemedView style={styles.container}>
 
       <View style={styles.question}>
-      <ThemedText type="title" style={styles.text}>Question</ThemedText>
+      <ThemedText type="title" style={styles.text}>{questions[0].question}</ThemedText>
       </View>
       <View style={styles.imagecontainer}>  
         <Image
-        source={require('@/assets/images/favicon.png')}
+        source={require('@/assets/images/image.png')}
       />
       </View>
-    
-      <ThemedText type="subtitle" style={styles.text}>HELLO WORLD</ThemedText>
-      <View style={styles.card}>
-      <Text>Sample Text</Text>
+      <View style={styles.option}>
+      
+      <Pressable style={({ pressed }) => [
+        styles.button,
+        pressed && styles.pressedState
+      ]}>
 
+      <ThemedText type="subtitle" style={styles.answertext}>answer 1</ThemedText>
+      
+      </Pressable>
+      <Pressable style={({ pressed }) => [
+        styles.button,
+        pressed && styles.pressedState
+      ]}> 
+        
+      <ThemedText type="subtitle" style={styles.answertext}>answer 2</ThemedText>
+      </Pressable>
       </View>
+            <View style={styles.option}>
+      <Pressable style={({ pressed }) => [
+        styles.button,
+        pressed && styles.pressedState
+      ]}> 
+      <ThemedText type="subtitle" style={styles.answertext}>answer 3</ThemedText>
+      </Pressable>
+      <Pressable style={({ pressed }) => [
+        styles.button,
+        pressed && styles.pressedState
+      ]}>
+        
+      <ThemedText type="subtitle" style={styles.answertext}>answer 4</ThemedText>
+      </Pressable>
+      </View>
+        
  
     </ThemedView>
   );
@@ -45,11 +76,28 @@ const styles = StyleSheet.create({
 
   },
   option: {
-
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 
   imagecontainer: {
     alignItems: 'center',
+    paddingVertical: 30,
+    paddingBottom: 60
 
+  },
+  answertext: {
+    color: '#000',
+    alignItems: 'center',
+    paddingHorizontal: 300,
+    paddingVertical: 50
+  },
+   button: {
+    padding: 10,
+    borderRadius: 5,
+  },
+  pressedState: {
+    opacity: 0.7, 
   },
 });
